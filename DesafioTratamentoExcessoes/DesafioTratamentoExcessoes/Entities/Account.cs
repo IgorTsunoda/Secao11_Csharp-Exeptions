27bde3dd3640d51ad74345492819ced5f3e8235c
Igor Tsunoda <igortaikitsunoda@gmail.com>
2024-01-19 13:49:10 -0300

    Desafio de Tratamento de Excessões concluido.

---
+++ DesafioTratamentoExcessoes/DesafioTratamentoExcessoes/Entities/Account.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using DesafioTratamentoExcessoes.Entities.Exeption;
+
+namespace DesafioTratamentoExcessoes.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if(Balance < amount )
+            {
+                throw new DomainException("Insuficient balance in account to do the withdraw.");
+            }
+            if(WithdrawLimit < amount)
+            {
+                throw new DomainException("Withdraw limit is not suficient to do the withdraw.");
+            }
+            Balance -= amount;
+        }
+    }
+}
---
+++ DesafioTratamentoExcessoes/DesafioTratamentoExcessoes/Program.cs
@@ -0,0 +1,39 @@
+﻿using DesafioTratamentoExcessoes.Entities;
+using DesafioTratamentoExcessoes.Entities.Exeption;
+using System.Diagnostics.Metrics;
+using System.Globalization;
+
+namespace DesafioTratamentoExcessoes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter account data: ");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Holder: ");
+            string holder = Console.ReadLine();
+            Console.Write("Initial balance: ");
+            double intialBalance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Withdraw limit: ");
+            double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Account c1 = new Account(number, holder, intialBalance, withdrawLimit);
+            
+            Console.WriteLine();
+            Console.Write("Enter amount for withdraw: ");
+            double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            try
+            {
+                c1.Withdraw(amount);
+                Console.WriteLine("New balance: " + c1.Balance.ToString("F2"), CultureInfo.InvariantCulture);
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Withdraw issue: " + e.Message);
+            }
+        }
+    }
+}
